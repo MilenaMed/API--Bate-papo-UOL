0e89a01b0de3eae68f277a5251232c6d143587e1
MilenaMed <milenamfm@gmail.com>
2023-04-17 12:48:23 -0300

    get menssages

--- src/app.js
+++ src/app.js
@@ -30,15 +30,14 @@ app.post("/participants", async (request, response) => {
 
     const usuario = request.body;
     const validacao = participantes.validate(usuario);
-    if (validacao.error) {
+    if (validacao.err) {
         return response.sendStatus(422)
     }
 
     //Novo usúario
     const nomeExiste = await db.collection("participants").findOne({ name: usuario })
-console.log(usuario.name)
     if (nomeExiste) {
-        return response.status(409).send("Nome de usuário em uso")
+        return response.sendStatus(409).send("Nome de usuário em uso")
     }
     try {
         await db.collection("participants").insertOne({
@@ -63,51 +62,61 @@ console.log(usuario.name)
 // GET - PARTICIPANTS
 app.get('/participants', async (request, response) => {
     const usuarios = await db.collection("participants").find().toArray()
-    return response.status(200).send(usuarios)
+    return response.sendStatus(200).send(usuarios)
 })
 
 // POST - MESSAGES
 app.post("messages", async (request, response) => {
     const mensagemEnviada = request.body;
-
     const validacaomensagem = mensagem.validate(mensagemEnviada)
 
     if (validacaomensagem.error) {
         return response.sendStatus(422)
     }
 
-    const nomeExiste = await db.collection("participants").findOne({ name: usuario })
+    const nomeExiste = await db.collection("participants").findOne({ name: usuario.name })
 
     if (!nomeExiste) {
-        return response.status(422).send("Nome de usuário não encontrado")
+        return response.sendStatus(422).send("Nome de usuário não encontrado")
+    }
+    try {
+        await db.collection("messages").insertOne({
+            from: usuario,
+            ...mensagemEnviada,
+            time: dayjs().format('HH:mm:ss')
+        })
+        return response.sendStatus(201).send("Mensagem enviada!");
+    } catch (err) {
+        return response.sendStatus(500).send(err.message)
     }
 })
 
 //GET - MESSAGES
 app.get('/messages', async (request, response) => {
-    const limite = request.query.limit
-    const limiteMax = parseInt(limite)
+    const limite = parseInt(request.query.limit)
+    const usuario = request.headers.user
+        const mensagens = await db.collection("messages").find({
+            $or: [
+                { to: "Todos" },
+                { to: usuario },
+                { from: usuario },
+                { type: "message" }
+            ]
+        }).toArray()
 
     try {
-        if (limit) {
-            if (limiteMax <= 0 || isNaN(limit)) {
+        if (limite) {
+            if (limite <= 0 || isNaN(limite)) {
                 return res.sendStatus(422)
             }
-
-            const usuario = request.headers.user
-            const mensagens = await db.collection("messages").find({$or: [
-                {to: "Todos"}, 
-                {to: usuario}, 
-                {from: usuario},
-                { type: "message" }
-            ]}).toArray();
-
-            return response.send([...mensagens].slice(-limit).reverse())
+        } else if (limite > 0) {
+            return response.send(mensagens.slice(-limit).reverse())
         }
+
         return response.sendStatus(200).send([...mensagens].reverse())
-    } catch(err) {
-        return response.sendStatus(422).send("Erro: Não foi possível pegar mensagens")
-    }
+} catch (err) {
+    return response.sendStatus(422).send("Erro: Não foi possível pegar mensagens")
+}
 })
 
 //POST- STATUS
